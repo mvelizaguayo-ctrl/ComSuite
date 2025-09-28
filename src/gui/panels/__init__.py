5d6d09f0212f523f6021ac87b69b4df6f43bf07f
mvelizaguayo-ctrl <mvelizaguayo@gmail.com>
2025-09-28 15:55:29 -0600

    Update __init__.py

--- src/gui/panels/__init__.py
+++ src/gui/panels/__init__.py
@@ -1,13 +1,9 @@
 from .device_panel import DevicePanel, SimpleDevicePanel
-from .connection_panel import ConnectionPanel
 from .data_monitor import DataMonitor, SimpleDataMonitor
-from .log_viewer import LogViewer
 
 __all__ = [
     'DevicePanel', 
     'SimpleDevicePanel',
-    'ConnectionPanel',
     'DataMonitor', 
-    'SimpleDataMonitor',
-    'LogViewer'
+    'SimpleDataMonitor'
 ]
