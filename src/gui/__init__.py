1ca75f66b5947ed22b6f259dec9eac0301853168
mvelizaguayo-ctrl <mvelizaguayo@gmail.com>
2025-09-28 15:55:36 -0600

    Update __init__.py

--- src/gui/__init__.py
+++ src/gui/__init__.py
@@ -3,12 +3,11 @@ from .modes.novice_mode import NoviceMode
 from .modes.expert_mode import ExpertMode
 from .wizards.device_wizard import DeviceWizard
 from .wizards.connection_wizard import ConnectionWizard
-from .panels.device_panel import DevicePanel
-from .panels.data_monitor import DataMonitor
+from .panels.device_panel import DevicePanel, SimpleDevicePanel
+from .panels.connection_panel import ConnectionPanel
+from .panels.data_monitor import DataMonitor, SimpleDataMonitor
 from .panels.log_viewer import LogViewer
 from .widgets.device_widget import DeviceWidget
-from .widgets.connection_widget import ConnectionWidget
-from .widgets.status_widget import StatusWidget
 from .style_manager import StyleManager
 
 __all__ = [
@@ -17,11 +16,12 @@ __all__ = [
     'ExpertMode',
     'DeviceWizard',
     'ConnectionWizard',
-    'DevicePanel',
-    'DataMonitor',
+    'DevicePanel', 
+    'SimpleDevicePanel',
+    'ConnectionPanel',
+    'DataMonitor', 
+    'SimpleDataMonitor',
     'LogViewer',
     'DeviceWidget',
-    'ConnectionWidget',
-    'StatusWidget',
     'StyleManager'
 ]
