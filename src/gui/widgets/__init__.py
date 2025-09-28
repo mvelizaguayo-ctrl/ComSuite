73efc563b5615403c133b788af29ea61ee2f059c
mvelizaguayo-ctrl <mvelizaguayo@gmail.com>
2025-09-28 15:55:07 -0600

    Update __init__.py

--- src/gui/widgets/__init__.py
+++ src/gui/widgets/__init__.py
@@ -1,5 +1,3 @@
 from .device_widget import DeviceWidget
-from .connection_widget import ConnectionWidget
-from .status_widget import StatusWidget
 
-__all__ = ['DeviceWidget', 'ConnectionWidget', 'StatusWidget']
+__all__ = ['DeviceWidget']
